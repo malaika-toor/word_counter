ed7c1ee8ecfe3a187df628badd0cb13fb8e1165c
malaika toor <164207619+malaika-toor@users.noreply.github.com>
2024-05-08 20:39:43 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const wordCount = (string) => {
+    const words = string.trim().split(/\s+/g);
+    console.log("Total Words In The Para Are: " + words.length);
+};
+const getInput = async () => {
+    const userInput = await inquirer.prompt([
+        {
+            type: "input",
+            name: "para",
+            message: "Enter your para:"
+        }
+    ]);
+    await wordCount(userInput.para);
+};
+await getInput();
---
+++ index.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import inquirer from "inquirer"; 
+
+type wordCount =(str: string) => void;
+
+const wordCount: wordCount =(string) => {
+    const words = string.trim().split(/\s+/g);
+    
+    console.log("Total Words In The Para Are: "+words.length);
+
+}
+
+const getInput = async() =>{
+    const userInput = await inquirer.prompt([
+        {
+           type:"input",
+           name:"para",
+           message:"Enter your para:"
+        }
+    ]);
+    await wordCount(userInput.para);
+
+
+}
+await getInput();
